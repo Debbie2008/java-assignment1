e648aa57042a169254e7473713d2cb85a5ea4314
Debbie2008 <yiduo5990@gmail.com>
2024-12-25 21:34:15 -0800

    Update StudentGradingSystem.java

--- StudentGradingSystem.java
+++ StudentGradingSystem.java
@@ -22,9 +22,9 @@ public class StudentGradingSystem {
 		double mark3 = b.nextDouble();
 		
 		double total = mark1 + mark2 + mark3;
-		System.out.println( name + "'s total score is " + total + ".");
-		
 		double average = total / 3;
+
+		System.out.println( name + "'s total score is " + total + ".");
 		System.out.println( name + "'s average score is " + average + ".");
 		
 		if (average >=90 && average <= 100) {
